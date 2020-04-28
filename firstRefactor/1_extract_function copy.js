11031ebeb6e038db871f9debaf13bb611e41a9f5
norvca <norvca@gmail.com>
2020-04-29 04:04:50 +0800

    feat: extract function

---
+++ firstRefactor/1_extract_function copy.js
@@ -0,0 +1,36 @@
+function printOwing(invoice) {
+  printBanner();
+  const outstanding = calculateOutstanding(invoice);
+  recordDueToday(invoice);
+  printDetails(invoice, outstanding);
+}
+
+function printBanner() {
+  console.log('***********************');
+  console.log('**** Customer Owes ****');
+  console.log('***********************');
+}
+
+function calculateOutstanding(invoice) {
+  let result = 0;
+  for (const o of invoice.orders) {
+    result += o.amount;
+  }
+
+  return result;
+}
+
+function recordDueToday(invoice) {
+  const today = Clock.today;
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
+}
+
+function printDetails(invoice, outstanding) {
+  console.log(`name: ${invoice.customer}`);
+  console.log(`amount: ${outstanding}`);
+  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+}
